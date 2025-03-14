a4e14d0f31fc0e583bf4a099186865689ef70cba
karthik-k44 <karthikkarunakaran44@gmail.com>
2025-03-14 16:35:32 +0530

    implement AddPassenger form with state management for input fields

--- frontend/vite-project/src/pages/AddPassenger.jsx
+++ frontend/vite-project/src/pages/AddPassenger.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddPassenger = () => {
+  const [passenger, setPassenger]= useState([{
+    name:"",
+    age:"",
+    gender:"",
+    contact:"",
+    email:"",
+
+  }])
+
+  const [passengers, setPassengers]= useState([])
+
+  const HandleEvent =(e)=>{
+    const {name, value}= e.target
+    setPassenger(prev=>({
+      ...prev,
+      [name]:value
+    }))
+  }
+
+    
+  const HandleSubmit= (e)=>{
+    e.preventDefault()
+    
+    console.log(passenger)
+  }
+  
+  console.log(passenger)
   return (
    <>
-    hello
+      <form action="" onSubmit={HandleSubmit}>
+        <label htmlFor="" >Name of Passenger : </label>
+        <input type="text"  name="name" value={passenger.name} onChange={HandleEvent} />
+        <br />
+        <br />
+        <label htmlFor="" >Age : </label>
+        <input type="text" name="age" value={passenger.age} />
+        <br />
+        <br />
+        <label htmlFor="" >Gender : </label>
+        <select name="gender" value={passenger.gender}>
+          <option value="Others"> Others</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+        <br />
+        <br />
+        <label htmlFor="">Contact number : </label>
+        <input type="text" name="contact" value={passenger.contact}/>
+        <br />
+        <br />
+        <label htmlFor="" > Email : </label>
+        <input type="email" name="email" value={passenger.email} />
+
+        <button type='submit'>Add passenger</button>
+      </form>
    </>
   )
+
+
 }
-
-
